7709907ad043fd2b4083b21cbaf4948c11d50fec
mtyrolski <michal.tyrolski@gmail.com>
2025-04-28 00:49:47 +0200

    todoist automations init local env

---
+++ todoist/automations/init_env.py
@@ -0,0 +1,27 @@
+import hydra
+from loguru import logger
+from tqdm import tqdm
+from omegaconf import DictConfig
+from todoist.automations.base import Automation
+from todoist.database.base import Database
+
+
+@hydra.main(version_base=None, config_path=None)
+def main(config: DictConfig) -> None:
+    # Setup logging to a file with rotation
+    logger.add("automation.log", rotation="500 MB")
+
+    dbio = Database('.env')
+    automations: list[Automation] = hydra.utils.instantiate(config.automations)
+    logger.info("Loaded automations: {}", list(map(str, automations)))
+    logger.info("Starting automations...")
+    for automation in tqdm(automations, desc="Processing automations"):
+        logger.info("Running automation: {}", automation)
+        automation.tick(dbio)
+        logger.info("Automation completed: {}", automation)
+    logger.success("All automations completed.")
+
+
+if __name__ == '__main__':
+    # pylint: disable=no-value-for-parameter
+    main()
