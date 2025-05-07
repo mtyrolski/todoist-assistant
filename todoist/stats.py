b8604e77694ac5d0f3b471d1f6a5953524d5bce1
mtyrolski <michal.tyrolski@gmail.com>
2025-05-07 19:35:08 +0200

    hotfix with priority

--- todoist/stats.py
+++ todoist/stats.py
@@ -11,14 +11,19 @@ def all_tasks(project: Project):
     return len(project.tasks)
 
 
-def priority_tasks(project: Project, prio: int):
-    return len([task for task in project.tasks if task.task_entry.priority == prio])
+def priority_tasks(project: Project, prio: int) -> int:
+    """
+    Returns the number of tasks with a given priority in a project.
+    :param project: The project to check.
+    :param prio: The priority to check for.
+    :return: The number of tasks with the given priority.
+    """
+    return len(list(filter(lambda task: task.task_entry.priority == prio, project.tasks)))
 
-
-p1_tasks = partial(priority_tasks, prio=1)
-p2_tasks = partial(priority_tasks, prio=2)
-p3_tasks = partial(priority_tasks, prio=3)
-p4_tasks = partial(priority_tasks, prio=4)
+p1_tasks = partial(priority_tasks, prio=4)
+p2_tasks = partial(priority_tasks, prio=3)
+p3_tasks = partial(priority_tasks, prio=2)
+p4_tasks = partial(priority_tasks, prio=1)
 
 
 def any_labels(project: Project):
