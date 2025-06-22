9c0944487fba0397ed3c2a5debaf587047cb7b5b
mtyrolski <michal.tyrolski@gmail.com>
2025-06-23 01:11:56 +0200

    make-update feature

---
+++ todoist/automations/update_env.py
@@ -0,0 +1,33 @@
+import hydra
+from loguru import logger
+from tqdm import tqdm
+from omegaconf import DictConfig
+from todoist.automations.base import Automation
+from todoist.database.base import Database
+from todoist.utils import try_n_times
+from functools import partial
+
+
+@hydra.main(version_base=None, config_path=None)
+def main(config: DictConfig) -> None:
+    # Setup logging to a file with rotation
+    logger.add("automation.log", rotation="500 MB")
+
+    dbio = Database('.env')
+    automations: list[Automation] = hydra.utils.instantiate(config.automations)
+    logger.info("Loaded automations: {}", list(map(str, automations)))
+    
+    # Filtering only for short ones
+    automation_gen = filter(lambda x: not x.is_long, automations)
+    
+    logger.info("Starting automations...")
+    for automation in tqdm(automation_gen, desc="Processing automations"):
+        logger.info("Running automation: {}", automation)
+        try_n_times(partial(automation.tick, dbio), 5)
+        logger.info("Automation completed: {}", automation)
+    logger.success("All automations completed.")
+
+
+if __name__ == '__main__':
+    # pylint: disable=no-value-for-parameter
+    main()
