b1156b3cf0f028c8972bdf7855d01e94c35dec10
mtyrolski <michal.tyrolski@gmail.com>
2025-08-12 02:31:21 +0200

    init env also only once executing activity

--- todoist/automations/init_env.py
+++ todoist/automations/init_env.py
@@ -6,7 +6,7 @@ from todoist.automations.base import Automation
 from todoist.database.base import Database
 from todoist.utils import try_n_times
 from functools import partial
-
+from todoist.automations.activity import Activity
 
 @hydra.main(version_base=None, config_path=None)
 def main(config: DictConfig) -> None:
@@ -16,6 +16,22 @@ def main(config: DictConfig) -> None:
     dbio = Database('.env')
     automations: list[Automation] = hydra.utils.instantiate(config.automations)
     logger.info("Loaded automations: {}", list(map(str, automations)))
+    
+    # if Activity among automations, we need to run the longest one.
+    activity_automations: list[Activity] = list(filter(lambda x: isinstance(x, Activity), automations))
+    rest_automations = list(filter(lambda x: not isinstance(x, Activity), automations))
+    if activity_automations:
+        longest_automation = max(activity_automations, key=lambda x: x.nweeks)
+        logger.info(f"Activity automations found, running the longest one - last {int(longest_automation.nweeks)} weeks of activity collection.")
+        automations = [longest_automation] + rest_automations
+    else:
+        logger.info("No activity automations found, running all remaining automations.")
+    
+    if not automations:
+        logger.warning("No automations to run. Exiting.")
+        return
+    
+
     logger.info("Starting automations...")
     for automation in tqdm(automations, desc="Processing automations"):
         logger.info("Running automation: {}", automation)
