49e57735968004c79162734a336744a34df7b7b8
mtyrolski <michal.tyrolski@gmail.com>
2026-01-02 02:33:36 +0100

    __main__ dropped

--- todoist/automations/multiplicate/__main__.py
+++
@@ -1,5 +0,0 @@
-from .automation import main
-
-
-if __name__ == "__main__":
-    main()
--- todoist/automations/template/__main__.py
+++
@@ -1,6 +0,0 @@
-from .automation import main
-
-
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    main()
